d6a5f201fbedd227a855b641489396fb5e45c40a
Natan Gabriel Almeida de castro <natsuaz27@gmail.com>
2023-03-07 14:28:17 -0300

    Update instalar.ps1

--- instalar.ps1
+++ instalar.ps1
@@ -1,5 +1,5 @@
-#winget install Microsoft.DotNet.SDK.7
-#winget install SQLite.SQLite
+winget install Microsoft.DotNet.SDK.7
+winget install SQLite.SQLite
 $caminho = $env:userprofile + '\bin\produto-curso'
 mkdir $caminho
 Copy-Item '.\Banco de dados\caixa.sql' $caminho
